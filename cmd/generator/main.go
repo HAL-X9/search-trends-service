ab787994d0e2ecc2a9d8ead816e96157de43b62f
HAL-X9 <JimiBlackmore@proton.me>
2026-05-24 11:58:35 +0300

    feat: добавлен генератор запросов в кафка для отправки тестовых логов в консьюмер

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -1,5 +1,64 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log/slog"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+var words = []string{
+	"платье женское", "кроссовки", "футболка", "сумка", "носки",
+	"джинсы", "купальник", "серьги", "чехол на айфон", "косметика",
+	"шорты", "босоножки", "рюкзак", "крем для лица", "худи", "реклама",
+	"спам_запрос", "купить_дешево_акция", "тест",
+}
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("starting search traffic generator...")
+
+	// Инициализация Franz-Kafka
+	cl, err := kgo.NewClient(
+		kgo.SeedBrokers("localhost:9092"),
+	)
+	if err != nil {
+		logger.Error("failed to create kafka client", "error", err)
+		os.Exit(1)
+	}
+	defer cl.Close()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	ticker := time.NewTicker(5 * time.Millisecond)
+	defer ticker.Stop()
+
+	logger.Info("generator successfully connected to kafka, broadcasting traffic...")
+
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Info("stopping generator gracefully...")
+			return
+		case <-ticker.C:
+			word := words[rand.Intn(len(words))]
+
+			record := &kgo.Record{
+				Topic: "search-logs",
+				Value: []byte(word),
+			}
+
+			cl.Produce(ctx, record, func(r *kgo.Record, err error) {
+				if err != nil {
+					logger.Error("failed to deliver message", "error", err)
+				}
+			})
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/HAL-X9/search-trends-service
 go 1.26.1
 
 require (
+	github.com/twmb/franz-go v1.21.2
 	golang.org/x/sync v0.20.0
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/klauspost/compress v1.18.6 // indirect
+	github.com/pierrec/lz4/v4 v4.1.26 // indirect
+	github.com/twmb/franz-go/pkg/kmsg v1.13.1 // indirect
 )
