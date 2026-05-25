5cb905afbf31033c329923045dc6a0dd65d7f307
HAL-X9 <JimiBlackmore@proton.me>
2026-05-25 11:19:47 +0300

    feat: добавлена логика защиты от спама и ботов

--- internal/usecases/anti_fraud.go
+++ internal/usecases/anti_fraud.go
@@ -1,1 +1,45 @@
 package usecases
+
+import (
+	"sync"
+	"time"
+)
+
+type AntiFraudDetector struct {
+	mu           sync.RWMutex
+	userRequests map[string]int
+	lastReset    time.Time
+	maxLimit     int
+}
+
+func NewAntiFraudDetector() *AntiFraudDetector {
+	return &AntiFraudDetector{
+		userRequests: make(map[string]int),
+		lastReset:    time.Now(),
+		maxLimit:     5,
+	}
+}
+
+// IsSpam проверяет не превысил ли лимит данный пользователь или IP-адрес
+func (af *AntiFraudDetector) IsSpam(event SearchEvent) bool {
+	af.mu.Lock()
+	defer af.mu.Unlock()
+
+	if time.Since(af.lastReset) >= time.Second {
+		af.userRequests = make(map[string]int)
+		af.lastReset = time.Now()
+	}
+
+	clientKey := event.UserID + ":" + event.Query
+	if event.UserID == "" {
+		clientKey = event.IPAddress + ":" + event.Query
+	}
+
+	af.userRequests[clientKey]++
+
+	if af.userRequests[clientKey] > af.maxLimit {
+		return true
+	}
+
+	return false
+}
